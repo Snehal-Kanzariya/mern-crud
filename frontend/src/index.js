da4b103015de07b1dea3acef5fae36326e2c8271
Snehal-Kanzariya <snehal.kanzariya02@gmail.com>
2023-02-06 08:29:11 +0530

    replace new router and folder structure update

---
+++ frontend/src/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar";
+import CreateStudentComponent from "./components/CreateStudentComponent";
+import StudentList from "./components/StudentList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+  },
+  {
+    path: "create-student",
+    element: (
+      <>
+        <Navbar />
+        <CreateStudentComponent />
+      </>
+    ),
+  },
+  {
+    path: "student-list",
+    element: (
+      <>
+        <Navbar />
+        <StudentList />
+      </>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
